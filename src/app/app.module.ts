d93db34135e1606d26ff6794e9fcf70c2f21333a
DavidCarlosSL <davidac4@live.com>
2020-12-23 10:51:16 -0300

    initial configuration

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GraphQLModule } from './core/graphql/graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    GraphQLModule,
+    HttpClientModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/core/graphql/graphql.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache,ApolloLink } from '@apollo/client/core';
+import { setContext } from '@apollo/client/link/context';
+
+const uri = 'http://localhost:3000/';
+
+export function createApollo(httpLink: HttpLink) {
+  const basic = setContext((operation, context) => ({headers: {Accept: 'charset=utf-8'}}));
+
+  const auth = setContext((operation, context) => {
+  const userToken = localStorage.getItem('userToken');
+  if (userToken === null) {
+    return {};
+  } else {
+    return {headers: {authorization: userToken}};
+  }
+  });
+
+  const link = ApolloLink.from([basic, auth, httpLink.create({ uri })]);
+  const cache = new InMemoryCache();
+
+  return {
+    link,
+    cache
+  }
+}
+
+@NgModule({
+  exports: [
+    HttpClientModule,
+  ],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory: createApollo,
+    deps: [HttpLink]
+  }]
+})
+export class GraphQLModule {}
